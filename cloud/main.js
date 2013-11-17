f99c89498a4dc0c5724d5355622c1d5e08e514fc
zerozaki0752 <albert@AlberttekiMacBook-Pro.local>
2013-11-17 12:28:38 +0800

    1

---
+++ cloud/main.js
@@ -0,0 +1,252 @@
+// Use AV.Cloud.define to define as many cloud functions as you want.
+// For example:
+AV.Cloud.define("hello", function(request, response) {
+    response.success("Hello world!");
+});
+//生成guid
+function newGuid()
+{
+    var guid = "";
+    for (var i = 1; i <= 32; i++){
+        var n = Math.floor(Math.random()*16.0).toString(16);
+        guid += n;
+        if((i==8)||(i==12)||(i==16)||(i==20))
+            guid += "-";
+    }
+    return guid;
+}
+
+//TV注册
+AV.Cloud.define('tv_register', function(request, response) {
+
+    console.log('TV注册');
+
+    var result = register(request,response,10,null,"tv");
+    var user = result["user"];
+    if (user)
+    {
+        response.success(user);
+    }
+    else
+    {
+        response.error(result["error"]);
+    }
+
+});
+
+//Phone注册(第一次绑定设备)
+AV.Cloud.define('phone_register', function(request, response) {
+
+    console.log('Phone注册');
+
+    var tvUsername = request.params.tvCode;
+    var userPhone;
+
+    var userQ = new AV.Query(User);
+    userQ.equalTo("username", tvUsername);
+//    userQ.include('userCount');
+//    userQ.include('userFavicon');
+    userQ.first().then(function(tvUser){
+
+        if (tvUser)
+        {
+            var result = register(request,response,10,null,"phone");
+            phoneUser = result["user"];
+            if (!phoneUser)
+            {
+                response.error(result["error"]);
+            }
+//            AV.Object.createWithoutData("_User", userPhone.id);
+            tvUser.relation('phones').add(phoneUser);
+            return tvUser.save();
+
+        }
+
+        }).then(function(user){
+
+            response.success(userPhone);
+
+         },function(error){
+
+            response.error(error);
+        });
+});
+
+//绑定设备
+AV.Cloud.define('phone_binding', function(request, response) {
+
+    relationOfPhoneTV(request, response, 1);
+
+});
+
+//解除绑定方法
+AV.Cloud.define('phone_binding', function(request, response) {
+
+    relationOfPhoneTV(request, response, 0);
+
+});
+
+
+var relationOfPhoneTV = function(request, response, isBinding) {
+
+    console.log('绑定设备');
+    var tvUsername = request.params.tvCode;
+    var phoneUsername = request.params.phoneCode;
+
+    var tvUser;
+    var phoneUser;
+
+    var userTQ = new AV.Query(User);
+    userTQ.equalTo("username", tvUsername);
+//    userQ.include('phone');
+//    userQ.include('userFavicon');
+    userTQ.first().then(function(user){
+
+        tvUser = user;
+
+        var result;
+        if (isBinding)
+        {
+            result = bindingPhoneToTV(tvUser,phoneUser);
+        }
+        else
+        {
+            result = unbindingPhoneToTV(tvUser,phoneUser);
+        }
+
+        if (result['user'])
+        {
+            response.success(result['user']);
+        }
+        else if (result['error'])
+        {
+            response.error(result['error']);
+        }
+
+    },function(error){
+
+        response.error(error);
+    });
+
+    var userPQ = new AV.Query(User);
+    userPQ.equalTo("username", phoneUsername);
+//    userQ.include('phone');
+//    userQ.include('userFavicon');
+    userPQ.first().then(function(user){
+
+        phoneUser = user;
+
+        var result;
+        if (isBinding)
+        {
+            result = bindingPhoneToTV(tvUser,phoneUser);
+        }
+        else
+        {
+            result = unbindingPhoneToTV(tvUser,phoneUser);
+        }
+
+        if (result['user'])
+        {
+            response.success(result['user']);
+        }
+        else if (result['error'])
+        {
+            response.error(result['error']);
+        }
+
+    },function(error){
+
+        response.error(error);
+    });
+}
+
+var bindingPhoneToTV = function(tvUser,phoneUser) {
+
+    if (tvUser && phoneUser)
+    {
+        tvUser.relation('phones').add(phoneUser);
+        tvUser.save().then(function(tvUser){
+            return  {'user':tvUser};
+        },function(error){
+            return  {'error':error};
+        });
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+var unbindingPhoneToTV = function(tvUser,phoneUser) {
+
+    if (tvUser && phoneUser)
+    {
+        tvUser.relation('phones').remove(phoneUser);
+        tvUser.save().then(function(tvUser){
+               return  {'user':tvUser};
+        },function(error){
+               return  {'error':error};
+        });
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+
+
+var register = function(request,response,count,error,type)
+{
+    if (count<=0) response.error(error);
+
+    var username = request.params.guid;
+
+    if (!username)
+    {
+        username = newGuid();
+    }
+
+    var email = username + "@qq.com";
+
+    if (username && password && email)
+    {
+        //创建用户关系
+//        var userRelation = new UserRelation();
+//        userRelation.save().then(function(userRelation){
+
+        var user = new AV.User();
+        user.set("username",username);
+        user.set("password", password);
+        user.set("email", email);
+        user.set('type',type);
+
+        user.signUp(null, {
+            success: function(user) {
+
+                var userRelation = new UserRelation();
+                user.set('userRelation', userRelation);
+
+                var userInfo = new UserInfo();
+                user.set('userInfo', userInfo);
+
+                user.save().then(function(user){
+
+//                    response.success(user);
+                    return {"user":user};
+
+                },function(error) {
+
+//                    response.error(error);
+                    return {"error":error};
+                });
+            },
+            error: function(user, error) {
+
+                return {"error":error};
+            }
+        });
+    }
+}
+
