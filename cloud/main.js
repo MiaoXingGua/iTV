b323f56128d35806022d5b6f4d231e453eb6941b
zerozaki0752 <albert@AlberttekiMacBook-Pro.local>
2013-11-18 16:28:21 +0800

    1

--- cloud/main.js
+++ cloud/main.js
@@ -38,6 +38,72 @@ AV.Cloud.define('phone_register', function(request, response) {
 
 });
 
+//全新注册
+AV.Cloud.define('register', function(request, response) {
+
+    console.log('注册');
+
+    register2(request,response,10,null);
+
+});
+
+var register2 = function(request,response,count,error)
+{
+    if (count<=0) response.error(error);
+
+    var username = request.params.guid;
+
+    if (!username)
+    {
+        username = newGuid();
+    }
+
+    var email = username + "@qq.com";
+
+    if (username && password && email)
+    {
+        //创建用户关系
+//        var userRelation = new UserRelation();
+//        userRelation.save().then(function(userRelation){
+
+        var user = new AV.User();
+        user.set("username",username);
+        user.set("password", password);
+        user.set("email", email);
+
+        user.signUp(null, {
+            success: function(user) {
+                console.log('注册3');
+
+                var userRelation = new UserRelation();
+                user.set('userRelation', userRelation);
+
+                var userInfo = new UserInfo();
+                user.set('userInfo', userInfo);
+
+                user.save().then(function(user){
+                    console.log('headView');
+                    //注册云通信
+                    cloopenSignUp(request, response, user);
+
+                },function(error) {
+
+                    console.log('注册6');
+                    response.error(error);
+                });
+
+            },
+            error: function(user, error) {
+                console.log('注册5');
+//                        console.log(error);
+//                register(response,--count,error);
+            }
+        });
+//        });
+
+    }
+}
+
 //绑定设备
 AV.Cloud.define('phone_binding', function(request, response) {
 
@@ -67,60 +133,60 @@ var relationOfPhoneTV = function(request, response, isBinding) {
     userTQ.equalTo("username", tvUsername);
 //    userQ.include('phone');
 //    userQ.include('userFavicon');
-    userTQ.first().then(function(user){
-
-        console.dir(user);
-        if (user.get('type') == 'tv')
-        {
-            tvUser = user;
-
-            if (isBinding)
+    userTQ.first({
+        success: function(user) {
+            console.dir(user);
+            if (user.get('type') == 'tv')
             {
-                bindingPhoneToTV(response,tvUser,phoneUser);
+                tvUser = user;
+
+                if (isBinding)
+                {
+                    bindingPhoneToTV(response,tvUser,phoneUser);
+                }
+                else
+                {
+                    unbindingPhoneToTV(response,tvUser,phoneUser);
+                }
             }
             else
             {
-                unbindingPhoneToTV(response,tvUser,phoneUser);
+                response.error(error+'不是tv的code');
             }
-        }
-        else
-        {
-            response.error(error+'不是tv的code');
-        }
 
-    },function(error){
-
-        response.error(error);
+        },
+        error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
     });
 
     var userPQ = new AV.Query(User);
     userPQ.equalTo("username", phoneUsername);
 //    userQ.include('phone');
 //    userQ.include('userFavicon');
-    userPQ.first().then(function(user){
-
-        console.dir(user);
-        if (user.get('type') == 'phone')
-        {
-            phoneUser = user;
-
-            if (isBinding)
+    userPQ.first({
+        success: function(user) {
+            if (user.get('type') == 'phone')
             {
-                bindingPhoneToTV(response,tvUser,phoneUser);
+                phoneUser = user;
+
+                if (isBinding)
+                {
+                    bindingPhoneToTV(response,tvUser,phoneUser);
+                }
+                else
+                {
+                    unbindingPhoneToTV(response,tvUser,phoneUser);
+                }
             }
             else
             {
-                unbindingPhoneToTV(response,tvUser,phoneUser);
+                response.error(error+'不是phone的code');
             }
-        }
-        else
-        {
-            response.error(error+'不是phone的code');
-        }
-
-    },function(error){
-
-        response.error(error);
+        },
+        error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+    }
     });
 }
 
@@ -209,6 +275,159 @@ var register = function(request,response,count,error,type)
             }
 
         });
+    }
+}
+
+//云通讯
+var crypto = require('crypto');
+var moment = require('moment');
+var Buffer = require('buffer').Buffer;
+
+function md5 (text)
+{
+    return crypto.createHash('md5').update(text).digest('hex');
+}
+
+function base64 (text)
+{
+    return new Buffer(text).toString('base64');
+}
+
+var parseString = require('xml2js').parseString;
+var parse = require('xml2js').Parser();
+
+
+//注册云通讯
+var cloopenSignUp = function(request, response, user)
+{
+    console.log('注册云通讯');
+//    console.log('注册云通讯' +user.id);
+
+    var timeStr = moment().format('YYYYMMDDHHmmss');
+//    console.log('timestr:' + timeStr);
+
+    var authorizationStr = 'aaf98f894032b237014047963bb9009d'+':'+timeStr;
+//    console.log('authorizationStr:' + authorizationStr);
+
+    var authorization64 = base64(authorizationStr);
+//    console.log('authorization64:' + authorization64);
+
+    var sigstr = 'aaf98f894032b237014047963bb9009d'+'bbc381b9a024443da462307cec93ce0b'+timeStr;
+//    console.log('sigstr:' + sigstr);
+
+    var sig = md5(sigstr);
+//    console.log('sig:' + sig    );
+
+    var bodyxml = '<?xml version="1.0" encoding="utf-8"?><SubAccount><appId>aaf98f894032b2370140482ac6dc00a8</appId><friendlyName>' + user.get('username') + '</friendlyName><accountSid>aaf98f894032b237014047963bb9009d</accountSid></SubAccount>';
+
+//    console.log('body:' + bodyxml);
+
+// console.log('url:https://sandboxapp.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase());
+// response.success('body:'+bodyxml);
+// response.success('https://sandboxapp.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase()),
+
+    AV.Cloud.httpRequest({
+        method: 'POST',
+        url: 'https://sandboxapp.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase(),
+        headers: {
+            'Content-Type': 'application/xml;charset=utf-8',
+            'Accept': 'application/xml',
+            'Authorization': authorization64
+        },
+        body: bodyxml,
+        success:function(httpResponse) {
+
+//            console.log(httpResponse.text);
+//            console.log(username);
+
+//            var xml = '<data>'+httpResponse.text+'<guid>'+username+'</guid>'+'</data>';
+//            console.log(xml);
+
+//            console.log('username0=' +currentUser.get('username'));
+//            console.log('注册云通讯1' +user.id);
+            parseString(httpResponse.text, function (error, result) {
+//                console.log('username1=' + currentUser.get('username'));
+                if (result)
+                {
+//                    console.log( '类型' +typeof (result) );
+//                    console.log('注册云通讯2' +user.id);
+
+                    cloopen2avos(request, response, user, result);
+                }
+                else
+                {
+//                    console.error('Request failed with response code ' + httpResponse.text);
+                    response.error('Request failed with response code ' + error);
+                }
+            });
+
+        },
+        error:function(httpResponse) {
+
+            console.error('Request failed with response code ' + httpResponse.text);
+            response.error('Request failed with response code ' + httpResponse.status);
+        }
+    });
+}
+
+var cloopen2avos = function(request, response, user, xmppInfo)
+{
+//    console.log('username2=' + currentUser.get('username'));
+//    console.log('ssss=' + user.id);
+
+    var subAccountSid = xmppInfo.Response.SubAccount[0].subAccountSid[0];
+    var subToken = xmppInfo.Response.SubAccount[0].subToken[0];
+    var voipAccount = xmppInfo.Response.SubAccount[0].voipAccount[0];
+    var voipPwd = xmppInfo.Response.SubAccount[0].voipPwd[0];
+
+    if (subAccountSid && subToken && voipAccount && voipPwd)
+    {
+//        var userInfo = new UserInfo();
+
+        var userId = AV.Object.createWithoutData("_User", user.id);
+        var userInfo = user.get('userInfo');
+//        console.log('asdads='+userInfo.id);
+        userInfo.set("user", userId);
+        userInfo.set("subAccountSid", subAccountSid);
+        userInfo.set("subToken", subToken);
+        userInfo.set("voipAccount", voipAccount);
+        userInfo.set("voipPwd", voipPwd);
+
+        userInfo.save().then(function(userInfo) {
+
+//            console.log('xxxxxxx='+userInfo.id);
+//            var userInfoId = AV.Object.createWithoutData("UserInfo", userInfo.id);
+//            user.set("userInfo",userInfoId);
+//            return user.save();
+//
+//             }).then(function(user) {
+
+//                console.log('zzz='+user.id);
+//                var dict = new Dictionary();
+//                dict.Add('guid',user.get('username'));
+//                dict.Add('subAccountSid',subAccountSid);
+//                dict.Add('subToken',subToken);
+//                dict.Add('voipAccount',voipAccount);
+//                dict.Add('voipPwd',voipPwd);
+
+            var dict = {'guid':user.get('username'),'password':password,'subAccountSid':subAccountSid,'subToken':subToken,'voipAccount':voipAccount,'voipPwd':voipPwd};
+
+//                console.dir(dict);
+//                console.log('dict2='+dict.toString());
+
+            response.success(dict);
+
+        }, function(response,error) {
+
+//                console.error(error);
+            response.error(error);
+
+        });
+    }
+    else
+    {
+        console.error('Request failed with response code ' + xmppInfo);
+        response.error('Request failed with response code ' + xmppInfo);
     }
 }
 
